befb055497291a3afe48e3cfb62bfbe2ded818a1
Michał Lula <lulamichal@gmail.com>
2015-04-24 16:09:43 +0200

    setup.py bugfixing

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ setup(
     url=u'https://github.com/michallula/marathon-proxy-manager',
     download_url=u'https://github.com/michallula/marathon-proxy-manager/tarball/{version}'.format(version=version),
     keywords=[u'marathon', u'mesos', u'nginx', u'load-balancing', u'reverse proxy'],
-    package_dir={u'': 'src'},
-    packages=[u'marathon_proxy_manager'],
+    package_dir={'': 'src'},
+    packages=['marathon_proxy_manager'],
     install_requires=[
         u'argparse',
         u'marathon',
